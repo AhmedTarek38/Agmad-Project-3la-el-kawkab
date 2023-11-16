c392f4a2d4d5821273c7efa627d45f47f07e01ab
ahmed tarek <ahmedtarekk38@gmail.com>
2023-11-16 14:56:48 +0200

    main func

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
+// Function to add two numbers //SUM Function
+int addNumbers(int a, int b) {
+    return a + b;
+}
 
-  return 0;
+int main() {
+    int num1, num2;
 
+    // Taking input from the user
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
 
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Calling the addNumbers function and printing the result
+    int result = addNumbers(num1, num2);
+    printf("The sum of %d and %d is: %d\n", num1, num2, result);
+
+    return 0;
 }
