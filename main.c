f48dd9ecbc974194025922cd2bb2828d5196d6ff
AhmedTarek38 <150917113+AhmedTarek38@users.noreply.github.com>
2023-11-16 19:47:45 +0200

    main.c

--- main.c
+++ main.c
@@ -4,23 +4,23 @@
 
 
 // Function to add two numbers //SUM Function
-int addNumbers(int a, int b) {
+int addNumbers(float a, float b) {
     return a + b;
 }
 
 int main() {
-    int num1, num2;
+   float num1, num2;
 
     // Taking input from the user
     printf("Enter the first number: ");
-    scanf("%d", &num1);
+    scanf("%f", &num1);
 
     printf("Enter the second number: ");
-    scanf("%d", &num2);
+    scanf("%f", &num2);
 
     // Calling the addNumbers function and printing the result
-    int result = addNumbers(num1, num2);
-    printf("The sum of %d and %d is: %d\n", num1, num2, result);
+    ifloat result = addNumbers(num1, num2);
+    printf("The sum of %f and %f is: %f\n", num1, num2, result);
 
     float z = 0.0;
     z = multi(2.1, 2.3);
