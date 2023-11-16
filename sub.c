31967c5176a91b947a54eff57daaae1257224e5f
youmna <youmna8383@yahoo.com>
2023-11-16 19:23:34 +0200

    last sub push

--- sub.c
+++ sub.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-float sub(float a, floatb);
+float sub(float a, float b);
 int main()
 {
 	float x,y,z;
 	printf("enter two numbers");
 	scanf("%f",&x);
-	scanf(""%f",&y);
+	scanf("%f",&y);
 	z=sub(x,y);
 	printf("%f",z);
 	
